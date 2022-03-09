ee858fe48ad76f453eb2c47958919827f456a535
Jun_Q <junlee36520620@gmail.com>
2022-03-09 23:36:57 +0900

    멤버 클래스 선언

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -3,6 +3,7 @@ package hello.core.member;
 import java.util.HashMap;
 import java.util.Map;
 
+// 회원을 조회하고 저장하는 메소드를 지정해둔 클래스
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
