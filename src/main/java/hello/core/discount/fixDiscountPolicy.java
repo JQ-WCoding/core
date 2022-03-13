e6a31ccec48b6903f4d6d0925d5e51d7e2c72380
Jun_Q <junlee36520620@gmail.com>
2022-03-13 18:52:12 +0900

    주문관리 및 테스트 케이스 작성

---
+++ src/main/java/hello/core/discount/fixDiscountPolicy.java
@@ -0,0 +1,16 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class fixDiscountPolicy implements DiscountPolicy {
+
+    // 1000원 할인 (고정 금액 할인)
+    private final int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        // VIP 이면 정책상 할인 1000원을 해주고 아닌경우 0원 할인(할인 없음)
+        return member.getGrade() == Grade.VIP ? discountFixAmount : 0;
+    }
+}
