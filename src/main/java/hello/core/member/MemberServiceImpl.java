fa9db9a8f2668da78e79b3d86974a208cab77dbd
Jun_Q <junlee36520620@gmail.com>
2022-03-14 18:14:49 +0900

    Bean을 사용하지 않은 코드

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 추후 @Autowired를 통해 생성자와 함께 Depency를 주입할 예정
+    // 현재는 순수 자바 코드를 이용하여 제작
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,11 +8,13 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
+    /* 프로세스 -> 주문을 내리기 위해서는 해당 회원의 등급을 확인하고, 등급에 맞는 회원의 할인정책을 적용한 주문내역을 내린다 */
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new fixDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 멤버를 저장소에서 ID값으로 찾아 할인정책을 확인하고 새로운 주문을 생성한다.
         Member member = memberRepository.findById( memberId );
         int discountPrice = discountPolicy.discount( member, itemPrice );
         // 새로운 주문 생성하여 반환
