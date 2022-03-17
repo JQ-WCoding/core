574464d75eb30d91476d3c4f883dc2d4b1e1bbb0
Jun_Q <junlee36520620@gmail.com>
2022-03-17 18:27:42 +0900

    DI를 이용한 OCP, DIP, SRP 원칙 지키기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -16,6 +15,8 @@ public class AppConfig {
     // 역할 분담을 확실하게 정한다.
     // 나머지 Impl 클래스들은 인터페이스만 고려해서 기능을 이용하면 원하는대로 인터페이스로부터 구현된 객체를 교체하여 사용할 수 있다.
     // 단지 우린 인터페이스만 확인하면서 개발하면 된다
+
+    // DIP, SRP 원칙을 준수하여 구현된 ServiceImpl 내에서는 외부에서의 변경에 전혀 신경쓰지 않고 작업이 가능하도록 한다
 
     // 생성자 주입
     public MemberService memberService() {
