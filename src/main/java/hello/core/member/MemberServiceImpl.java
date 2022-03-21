7e596557181b61689207fcf97b354239f11c72a0
Jun_Q <junlee36520620@gmail.com>
2022-03-21 23:53:39 +0900

    구현

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,6 +7,7 @@ public class MemberServiceImpl implements MemberService {
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     // ServiceImpl 구현된 클래스들은 모두 실구현객체를 몰라도 된다.
     // 즉 인터페이스의 기능 설명만 보고 사용하더라도 문제없도록 하는 것이 SRP라는 단일책임 원칙과 OCP 개방폐쇄 원칙을 지켜 사용한 것이다.
+    // 멤버 서비스는 MemoryMemberRepositry에서 부터 구현되어 있는 메소드들을 사용한다
     private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
