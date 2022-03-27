f2dfb2809e017908d49692020d6e2f2d32fb0ba2
Jun_Q <junlee36520620@gmail.com>
2022-03-27 22:35:28 +0900

    @Bean 설명

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -21,8 +21,11 @@ public class OrderApp {
         // MemberService memberService = appConfig.memberService();
         // OrderService orderService = appConfig.orderService();
 
+        // ApplicationContext 를 사용해 스프링에 @Bean에 등록된 것을 스프링 컨테이너에서부터 불러온다.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext( AppConfig.class );
 
+        // 메소드와 같은 명칭의 memberService, orderService 로 작성하여 key, vlaue 와 같이 가지고 오는 형식
+        // @Bean(name = "")을 이용해 Name에 명칭을 작성할 순 있지만 관례상 기본값 그대로 사용한다
         MemberService memberService = applicationContext.getBean( "memberService", MemberService.class );
         OrderService orderService = applicationContext.getBean( "orderService", OrderService.class );
 
