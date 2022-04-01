f762842f731c44a5a8c36ce46f9d090c23f7ced8
Jun_Q <junlee36520620@gmail.com>
2022-04-01 23:54:53 +0900

    bean 확인

--- src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -23,14 +23,19 @@ class ApplicationContextInfoTest {
     @DisplayName ( "어플리케이션 빈 출력" )
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        // 향상 for문
         for ( String beanDefinitionName : beanDefinitionNames ) {
+            // bean 에 등록되어 있는 명칭 중에 정의 된 bean 찾기
             BeanDefinition beanDefinition = ac.getBeanDefinition( beanDefinitionName );
 
             // 빈이 역할을 하는지 여부를 물어보기
             // ROLE_APPLICATION : 직접 등록한 어플리케이션 빈
             // ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            // 빈의 역할이 직접 등록인지 스프링 내부에서 관리하는 형식인지 enum 으로 등록되어 있는 값을 이용해 찾는다
             if ( beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                // bean 에 정의된 이름으로 객체 생성하기
                 Object bean = ac.getBean( beanDefinitionName );
+                // 객체 반환 및 이름 확인하기
                 // 내가 등록한 Bean만 보기
                 System.out.println( "name = " + beanDefinitionName + " object = " + bean );
             }
