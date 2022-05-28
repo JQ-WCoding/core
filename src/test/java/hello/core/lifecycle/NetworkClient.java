d67fc518e52dac7568fd9ea6fe8009a3bf929e70
Jun_Q <junlee36520620@gmail.com>
2022-05-28 22:32:05 +0900

    Init destroy 클래스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -24,5 +27,19 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println( "close : " + url );
+    }
+
+    // 의존관계 주입이 끝나면
+    @PostConstruct
+    public void init() {
+        System.out.println( "NetworkClient.init" );
+        connect();
+        call( "초기화 연결 메세지" );
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println( "NetworkClient.close" );
+        disconnect();
     }
 }
