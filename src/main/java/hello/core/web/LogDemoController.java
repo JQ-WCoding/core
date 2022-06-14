cc3bceff71c4e48b8e35dfb44f976480d6ee6911
Jun_Q <junlee36520620@gmail.com>
2022-06-14 23:28:58 +0900

    url 확인

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,17 +14,21 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
+
+    // ObjectProvider를 사용하여 객체를 호출하는 시점에도 Bean 생성을 추적하고 지연할 수 있음
     private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping ( "log-demo" )
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestUrl = request.getRequestURL()
                 .toString();
+        // 현 타이밍에 getObject()를 호출하여 주입
         MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL( requestUrl );
 
         myLogger.log( "Controller Test" );
+        Thread.sleep( 100 );
         logDemoService.logic("testID");
         return "OK";
     }
